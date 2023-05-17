9cc4be897f147fa469b3ccd61230f007128e9f19
Deni July <denijuli.setiawan@gmail.com>
2023-05-17 22:32:55 +0700

    feat:set meta og dan tambah section highlight project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,36 @@
 module.exports = {
-	content: ["./index.html"],
-	theme: {
-		extend: {
-			container: {
-				padding: {
-					DEFAULT: "1rem",
-					sm: "2rem",
-					lg: "4rem",
-					xl: "5rem",
-					"2xl": "6rem",
-				},
-			},
-			colors: {
-				primary: "#0779E4",
-				secondary: "#FCBF1E",
-				black: "#333333",
-				"light-primary": "#EDF6FF",
-				"light-secondary": "#FFF6DE",
-				"dark-black": "#222831",
-				"dark-black-light": "#393E46",
-				"dark-black-light-2": "#73777C",
-			},
-			fontFamily: {
-				quicksand: ["Quicksand"],
-			},
-		},
-	},
-	darkMode: "class",
-	plugins: [],
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      container: {
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+      colors: {
+        primary: "#0779E4",
+        secondary: "#FCBF1E",
+        black: "#333333",
+        "light-primary": "#EDF6FF",
+        "light-secondary": "#FFF6DE",
+        "dark-black": "#222831",
+        "dark-black-light": "#393E46",
+        "dark-black-light-2": "#73777C",
+        "card-project":
+          "linear-gradient(123.01deg, #0779E4 -1.08%, rgba(255, 255, 255, 0) 30.59%),linear-gradient(297.91deg, #0779E4 -2.83%, rgba(255, 255, 255, 0) 57.85%)",
+      },
+      fontFamily: {
+        quicksand: ["Quicksand"],
+      },
+      boxShadow: {
+        "3xl": "0px 10px 30px rgba(0,0,0,0.03)",
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [],
 };
